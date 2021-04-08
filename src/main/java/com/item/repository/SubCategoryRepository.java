5f024ba1fe8e80c0ffd1f90b960aab07c4c95d64
Abhishek Khelge <abhikhelge21@gmail.com>
2021-04-09 01:34:47 +0530

    added repository

---
+++ src/main/java/com/item/repository/SubCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.item.repository;
+
+import com.item.entity.SubCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubCategoryRepository extends JpaRepository<SubCategory,Integer> {
+}
